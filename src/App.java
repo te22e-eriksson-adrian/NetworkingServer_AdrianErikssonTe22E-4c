7043916e2d4cb39e0fdd3f903e7b5164958413fe
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2025-05-06 11:03:25 +0200

    påbörjade skapandet av servern genom att implementera serverkoden från instruktionerna.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.net.*;
+import java.io.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        ServerSocket sever = null;
+        Socket client;
+
+        int portnumber = 21;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
     }
 }

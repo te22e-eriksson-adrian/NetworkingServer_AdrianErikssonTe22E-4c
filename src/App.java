79b029191043aa8498fc7aa8db023261bcee01c9
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2025-05-06 11:08:35 +0200

    lade till en trycatch-sats enligt samma instruktioner samt rättade till ett stavfel för "server" objektet.

--- src/App.java
+++ src/App.java
@@ -5,12 +5,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         
-        ServerSocket sever = null;
+        ServerSocket server = null;
         Socket client;
 
         int portnumber = 21;
         if (args.length >= 1) {
             portnumber = Integer.parseInt(args[0]);
         }
+
+        try {
+            server = new ServerSocket(portnumber);
+        } catch (IOException ie) {
+            System.out.println("Cannot open socket." + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created " + server);
     }
 }
